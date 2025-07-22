302f4fa347aa7ddac3e522d2bb661941fe3a9fb6
Mohammad Saber Mahmud Sourav <74147777+sabermh46@users.noreply.github.com>
2025-07-23 00:58:31 +0600

    bcend

--- backend/routes/fonts.js
+++ backend/routes/fonts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const FontController = require('../controllers/FontController');
+const FontController = require('../controllers/fontController');
 const FontRepository = require('../repositories/fontRepository');
 const FontService = require('../services/fontService');
 
